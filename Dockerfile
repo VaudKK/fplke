38bfa77e7814a8849b837d1d63f4f532e635706e
VaudKK <vaudkeith@gmail.com>
2024-06-06 19:22:35 +0300

    fix: handling BadCredentials Error

--- Dockerfile
+++ Dockerfile
@@ -12,36 +12,3 @@ COPY --from=build /app/target/*.jar ms-authentication.jar
 EXPOSE 8080
 
 ENTRYPOINT ["java","-Dspring.profiles.active=production","-jar","ms-authentication.jar"]
-
-
-##
-## Build stage
-##
-#FROM openjdk:17-jdk-slim AS build
-#COPY . .
-#RUN mvn clean package -DskipTests
-#FROM openjdk:17-jdk-slim
-#COPY --from=build /target/*.jar ms-authentication.jar
-#ENTRYPOINT ["java","-Dspring.profiles.active=production","-jar","rest-api-template.jar"]
-#
-#
-#COPY src /home/app/src
-#COPY pom.xml /home/app
-#RUN mvn -f /home/app/pom.xml clean package
-#
-##
-## Package stage
-##
-#FROM openjdk:17-jdk-slim
-#COPY --from=build /home/app/target/*.jar /usr/local/lib/ms-authentication.jar
-#EXPOSE 8080
-#ENTRYPOINT ["java","-jar","/usr/local/lib/ms-authentication.jar"]
-#
-#
-#
-#FROM openjdk:17-jdk-slim AS build
-#COPY . .
-#RUN mvn clean package -DskipTests
-#FROM openjdk:17-jdk-slim
-#COPY --from=build /target/*.jar ms-authentication.jar
-#ENTRYPOINT ["java","-Dspring.profiles.active=production","-jar","ms-authentication.jar"]
--- src/main/java/com/fplke/msauthentication/exceptions/handler/RestExceptionHandler.java
+++ src/main/java/com/fplke/msauthentication/exceptions/handler/RestExceptionHandler.java
@@ -1,18 +1,15 @@
 package com.fplke.msauthentication.exceptions.handler;
 
-import com.fplke.msauthentication.client.dto.response.FplEntryResponse;
 import com.fplke.msauthentication.exceptions.EmailExistsException;
 import com.fplke.msauthentication.exceptions.TeamIdException;
 import com.fplke.msauthentication.exceptions.TeamIdExistsException;
 import com.fplke.msauthentication.exceptions.TeamIdNotFoundException;
 import com.fplke.msauthentication.exceptions.model.ApiError;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
@@ -20,10 +17,8 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
 
     @ExceptionHandler(value = { BadCredentialsException.class })
-    protected ResponseEntity<Object> handleBadCredentialsException(BadCredentialsException ex, HttpHeaders headers,
-                                                                   HttpStatus status,
-                                                                   WebRequest request) {
-        var apiError = new ApiError(status,"Invalid credentials",ex.getLocalizedMessage());
+    protected ResponseEntity<Object> handleBadCredentialsException(BadCredentialsException ex) {
+        var apiError = new ApiError(HttpStatus.UNAUTHORIZED,"Invalid credentials",ex.getLocalizedMessage());
         return buildResponseEntity(apiError);
     }
 
