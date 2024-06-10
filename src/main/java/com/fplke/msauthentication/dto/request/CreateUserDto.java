d46ebb07f026716d54450decdb3ac86ac8d06c38
VaudKK <vaudkeith@gmail.com>
2024-06-10 10:48:51 +0300

    fix: setting user name min length to 3

--- src/main/java/com/fplke/msauthentication/dto/request/CreateUserDto.java
+++ src/main/java/com/fplke/msauthentication/dto/request/CreateUserDto.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
-public record CreateUserDto(@Pattern(regexp = "[A-Za-z0-9_\\s]{5,70}",
+public record CreateUserDto(@Pattern(regexp = "[A-Za-z0-9_\\s]{3,70}",
                             message = Constants.Strings.INVALID_USERNAME) String username,
                             @Email String email,
                             @NotNull Long teamId,
